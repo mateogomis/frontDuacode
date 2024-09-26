7320c30796bb7b5dfc1f453c48b9b5ac8b2cfaac
unknown <acontrerasregueiro@gmail.com>
2024-09-26 17:21:50 +0200

    corregido mostrar fotos en desarrollo

--- Backend/entorno_virtual/proyecto_duacode/proyecto_duacode/settings.py
+++ Backend/entorno_virtual/proyecto_duacode/proyecto_duacode/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
 ####################################################################
 # Nuestros documentos se guardaran en el directorio /media/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Ajusta esta ruta si es necesario
 
 ##################################################################
 
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
