6c1599482fcac4fa98ace4fb1b82d4b6c4d05f65
mateogomis <mateoopg01@gmail.com>
2024-10-17 10:33:56 +0200

    Eliminando backend

--- Backend/backend_duacode-main/entorno_virtual/proyecto_duacode/subir_archivo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubirArchivoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'subir_archivo'
--- Backend/backend_duacode-main/entorno_virtual/proyecto_duacode/subir_archivo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import SubirArchivo
-
-class FileUploadForm(forms.ModelForm):
-    class Meta:
-        model = SubirArchivo
-        fields = ['file','descripcion']
