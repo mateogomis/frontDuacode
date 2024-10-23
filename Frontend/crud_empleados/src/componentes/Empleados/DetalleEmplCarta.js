40f8ca68021fa7935628e1d1d73b547b8748a42a
MartinOisRod <martin.ois.rookie@gmail.com>
2024-10-23 13:13:58 +0200

    Arreglo DetalleProyectos

--- Frontend/crud_empleados/src/componentes/Empleados/DetalleEmplCarta.js
+++ Frontend/crud_empleados/src/componentes/Empleados/DetalleEmplCarta.js
@@ -11,10 +11,10 @@ const DetalleEmplCarta = ({ empleadoDetail }) => {
       <h2>{empleadoDetail.nombre} {empleadoDetail.apellido_1} {empleadoDetail.apellido_2}</h2>
       <p><strong>Email:</strong> {empleadoDetail.email}</p>
       <p><strong>Teléfono:</strong> {empleadoDetail.telefono}</p>
-      <p><strong>Puesto:</strong> {empleadoDetail.puesto}</p>
+      <p><strong>Puesto:</strong> {empleadoDetail.rol.nombre}</p>
       <p><strong>Fecha de Contratación:</strong> {empleadoDetail.fecha_contratacion}</p>
       <p><strong>Cumpleaños:</strong> {empleadoDetail.cumpleaños}</p>
-      <p><strong>Estado:</strong> {empleadoDetail.is_on_leave ? 'Está de baja' : 'No está de baja'}</p>
+      <p><strong>Estado: </strong>{empleadoDetail.is_on_leave ? 'Está de baja' : 'No está de baja'}</p>
     </div>
   );
 };
