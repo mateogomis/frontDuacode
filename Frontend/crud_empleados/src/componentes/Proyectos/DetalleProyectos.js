06e82fbeae2e27efbad0f425c93b40fce4e084a5
MartinOisRod <martin.ois.rookie@gmail.com>
2024-10-23 18:33:59 +0200

    rama Martin2

--- Frontend/crud_empleados/src/componentes/Proyectos/DetalleProyectos.js
+++ Frontend/crud_empleados/src/componentes/Proyectos/DetalleProyectos.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import '../../styles/DetalleProyectos.css';
-
+//detalle proy
 const DetalleProyectos = () => {
     const { id } = useParams();
     const [ProyectoDetail, setProyectoDetail] = useState(null);
