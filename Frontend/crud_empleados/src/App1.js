815c057b9e15a54820ec434f56fc056f2595ecdd
acontrerasregueirodev <adrian.contreras.rookie@gmail.com>
2024-10-23 11:03:34 +0200

    abre webcam y cierra webcam al hacer login/logout

---
+++ Frontend/crud_empleados/src/App1.js
@@ -0,0 +1,27 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import KioskoDuacode from './componentes/Kiosko/KioskoDuacode'; // Si aún lo necesitas
+import PanelEmpleados from './componentes/Kiosko/PanelEmpleados';
+
+function App() {
+  return (
+    <Router>
+      <div className="content-wrapper"> {/* Contenedor para el contenido */}
+        <Routes>
+          <Route path="/empleados" element={<PanelEmpleados />} />
+          <Route path="/" element={<KioskoDuacode />} /> {/* Muestra KioskoDuacode en la ruta principal */}
+          {/* Agrega más rutas aquí según sea necesario */}
+          <Route path="/proyectos" element={<h2>Lista de Proyectos</h2>} />
+          <Route path="/sedes" element={<h2>Lista de Sedes</h2>} />
+          <Route path="/salas" element={<h2>Lista de Salas</h2>} />
+          {/* <Route path="/perfil/:id" component={Perfil} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
